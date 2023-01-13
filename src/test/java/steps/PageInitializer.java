d350e2c66eefe594987d849be47a7fde2f7035b2
moazzamsadiq <moazzam@syntaxtechs.com>
2023-01-12 20:18:29 -0500

    resolved errors

--- src/test/java/steps/PageInitializer.java
+++ src/test/java/steps/PageInitializer.java
@@ -1,18 +1,23 @@
 package steps;
 
 
+import pages.AddEmployeePage;
+import pages.DashboardPage;
+import pages.EmployeeListPage;
+import pages.LoginPage;
+
 public class PageInitializer {
 
-    public static loginpage login;
-    public static dashboardPage dashboard;
-    public static addEmployeePage addEmployee;
-    public static employeeListPage employeeList;
+    public static LoginPage login;
+    public static DashboardPage dashboard;
+    public static AddEmployeePage addEmployee;
+    public static EmployeeListPage employeeList;
 
 
     public static void intializePageObjects(){
-        login = new loginPage();
-        dashboard = new dashboardPage();
-        addEmployee = new addEmployeePage();
-        employeeList = new employeeListPage();
+        login = new LoginPage();
+        dashboard = new DashboardPage();
+        addEmployee = new AddEmployeePage();
+        employeeList = new EmployeeListPage();
     }
 }
--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,6 +1,18 @@
 package utils;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import steps.PageInitializer;
+
+import java.util.concurrent.TimeUnit;
 
 public class CommonMethods extends PageInitializer {
 
@@ -23,7 +35,7 @@ public class CommonMethods extends PageInitializer {
        driver.manage().window().maximize();
        driver.get(ConfigReader.getPropertyValue("url"));
        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
-//     make sure to initalize the page objects at this point
+        intializePageObjects();
     }
 
     public static void closeBrowser(){
